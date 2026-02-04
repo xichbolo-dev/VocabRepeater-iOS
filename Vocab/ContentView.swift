e9901f7baebcfdfb399847a146fc73f61027136b
sunman <xichbolo@gmail.com>
2026-02-04 10:05:51 +0700

    Initial Commit

---
+++ Vocab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vocab
+//
+//  Created by sunman on 4/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vocab/VocabApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabApp.swift
+//  Vocab
+//
+//  Created by sunman on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct VocabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
